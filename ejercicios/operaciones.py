11f07a53b99ca8ae8243b914b6be4235a4196c8a
Jose Manuel Nebot Bosch <jm.nebotbosch@edu.gva.es>
2024-02-13 12:35:01 +0100

    test es_par

--- ejercicios/operaciones.py
+++ ejercicios/operaciones.py
@@ -1,5 +1,8 @@
 # En principio el alumno solo recibiría la definición de la función en el archivo correspondiente
-def suma(x,y):
-
+def es_par(x):
+    par = False
     # El alumno deberá implementar correctamente el código de la función para pasar el test correspondiente
-    return x + y
+    if x % 2 == 0:
+        par = True
+    return par
+        
